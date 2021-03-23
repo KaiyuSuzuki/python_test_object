ee1825191d753b50bca564715da06312de7e181b
kaiyu@local <kaiyu.wing@gmial.com>
2021-03-23 09:22:42 +0900

    prototype of test

--- test.py
+++ test.py
@@ -1,3 +1,29 @@
-"""
-このファイルに解答コードを書いてください
-"""
+import numpy as np
+
+
+def main(file_name):
+    with open(file_name) as f:
+        lines = f.readlines()
+    lines = [line.replace("\n", "").split(":") for line in lines]
+    i_list = [int(line[0]) for line in lines[:-1]]
+    s_list = [line[1] for line in lines[:-1]]
+    m = int(lines[-1][0])
+    indices = np.argsort(i_list)
+
+    ret = ""
+    for index in indices:
+        i = i_list[index]
+        s = s_list[index]
+
+        if m % i == 0:
+            ret += s
+
+    if ret == "":
+        ret = str(m)
+
+    return ret
+
+if __name__ == '__main__':
+    file_name = "input.txt"
+    ret = main(file_name)
+    print(ret)
